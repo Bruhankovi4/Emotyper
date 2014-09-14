4239c0c989b119c100fa6016ded91c1f2c422b2a
Grok <Bruhankovi4@gmail.com>
2014-09-14 16:17:25 +0300

    not tested implementation for inputSeriesBlock

--- Emotyper/WaveletStudio/Blocks/InputSeriesBlock.cs
+++ Emotyper/WaveletStudio/Blocks/InputSeriesBlock.cs
@@ -11,6 +11,7 @@ namespace WaveletStudio.Blocks
     [Serializable]
     public class InputSeriesBlock : BlockBase
     {
+        private List<double> _series = new List<double>(){1,2,3,4,5}; 
       /// <summary>
         /// Constructor
         /// </summary>
@@ -18,9 +19,26 @@ namespace WaveletStudio.Blocks
         {
             BlockBase root = this;
             CreateNodes(ref root);
-           
+          Start = 0;
+          Finish = 100;
+          SamplingInterval = 1;
+          SamplingRate = 1;
+
         }
 
+        public List<double> GetSeries()
+        {
+            return _series;
+        }
+        public void SetSeries(List<double> series )
+        {
+            this._series = series;
+        }
+        public void SetSeriesAndExecute(List<double> series)
+        {
+            this._series = series;
+            Execute();
+        }
         /// <summary>
         /// Name
         /// </summary>
@@ -45,7 +63,19 @@ namespace WaveletStudio.Blocks
         /// </summary>
         public override void Execute()
         {
-                  return;
+                  if (_series == null || !_series.Any())   //series is empty
+                  {
+                      return;
+                  }
+                  OutputNodes[0].Object = new List<Signal> { new Signal(_series.ToArray())
+                        {
+                            SamplingRate = SamplingRate,
+                            Start = Start,
+                            Finish = Finish,
+                            SamplingInterval = SamplingInterval
+                        }};
+                  if (Cascade && OutputNodes[0].ConnectingNode != null)
+                      OutputNodes[0].ConnectingNode.Root.Execute();    
         }
 
         /// <summary>
@@ -79,5 +109,13 @@ namespace WaveletStudio.Blocks
             block.Execute();
             return block;
         }
+
+        public int SamplingRate { get; set; }
+
+        public double Start { get; set; }
+
+        public double Finish { get; set; }
+
+        public double SamplingInterval { get; set; }
     }
 }
