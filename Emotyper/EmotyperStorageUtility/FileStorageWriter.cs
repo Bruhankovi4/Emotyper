fe15ff634019589dc2c2d623e813a77be8e09455
Grok <Bruhankovi4@gmail.com>
2014-09-16 11:35:32 +0300

    renaming of the project, added headsetDataProvider

--- Emotyper/EmotyperStorageUtility/FileStorageWriter.cs
+++ Emotyper/EmotyperStorageUtility/FileStorageWriter.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using Emotiv;
 
-namespace EmotyperStorageUtility
+namespace EmotyperDataUtility
 {
     public class FileStorageWriter
     {
